29e5c1a078b76f47757fe20bd2efcf80f6513988
Alexander <allemeshov@gmail.com>
2021-05-04 22:15:08 +0300

    poebat

--- MemoryCardsWebApp/ClientApp/src/app/components/training-home/training-home.component.ts
+++ MemoryCardsWebApp/ClientApp/src/app/components/training-home/training-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Card} from "../../interfaces/card.interface";
 
 @Component({
@@ -8,11 +8,38 @@ import {Card} from "../../interfaces/card.interface";
 })
 export class TrainingHomeComponent implements OnInit {
 
+  hasTurned = false;
+
+  get btnText(): string {
+    return this.hasTurned ? 'Отправить в середину' : 'Отправить в конец';
+  }
+
+
   @Input() cards: Card[];
+  @Output() closeEvent = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get cardText(): string {
+    return this.hasTurned ? this.cards[0].backText : this.cards[0].frontText;
+  }
+
+  hide(): void {
+    this.closeEvent.emit();
+  }
+
+  sendToMiddle(): void {
+
+  }
+
+  sendToBack(): void {
+
+  }
+
+  turnCard() {
+    this.hasTurned = !this.hasTurned;
+  }
 }
