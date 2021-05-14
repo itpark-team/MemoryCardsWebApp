ae97eecf53c51028c91a0aa2aae34c4bfdda6c87
Vsageev <sageevlad@yandex.ru>
2021-05-14 20:42:21 +0300

    wab lab dab dab dab

--- MemoryCardsWebApp/ClientApp/src/app/components/project-home/project-home.component.ts
+++ MemoryCardsWebApp/ClientApp/src/app/components/project-home/project-home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, } from '@angular/core';
 import {Router} from "@angular/router";
+import {waitForAsync} from "@angular/core/testing";
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-project-home',
@@ -8,12 +10,46 @@ import {Router} from "@angular/router";
 })
 export class ProjectHomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  constructor(private router: Router, private elementRef:ElementRef) {
   }
 
-  goToAuth(){
+
+  ngOnInit(): void {
+
+
+    this.fuckingUpdate();
+  }
+
+  // ngAfterViewInit() {
+  //   var s = document.createElement("script");
+  //   s.type = "text/javascript";
+  //   s.src = "https://code.jquery.com/jquery-3.5.1.min.js";
+  //   this.elementRef.nativeElement.appendChild(s);
+  //   var s2 = document.createElement("script");
+  //   s2.type = "text/javascript";
+  //   s2.src = "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js";
+  //   this.elementRef.nativeElement.appendChild(s2);
+  // }
+
+  async fuckingUpdate() {
+    while (true)
+    {
+      await this.delay(100);
+    }
+
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+
+  get GetScrollHeight() {
+    return document.getElementById("screen1").getBoundingClientRect().top;
+  }
+
+  goToAuth() {
     this.router.navigateByUrl("/auth");
   }
 
